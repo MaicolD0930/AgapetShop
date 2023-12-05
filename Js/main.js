f8d489fca06027bfa268292e26653b2223a8ad43
MaicolD0930 <maicold0930@gmail.com>
2023-12-04 19:53:55 -0500

    Se arregla alerta

--- Js/main.js
+++ Js/main.js
@@ -112,22 +112,10 @@ function agregarAlCarrito(e) {
     Toastify({
         text: "Producto agregado",
         duration: 3000,
-        close: true,
-        gravity: "top", // `top` or `bottom`
-        position: "right", // `left`, `center` or `right`
-        stopOnFocus: true, // Prevents dismissing of toast on hover
-        style: {
-            background: "linear-gradient(to right, #458942,#528443)",
-            borderRadius: "2rem",
-            textTransform: "uppercase",
-            fontSize: ".75rem"
-        },
-        offset: {
-            x: '1.5rem', // horizontal axis - can be a number or a string indicating unity. eg: '2em'
-            y: '1.5rem' // vertical axis - can be a number or a string indicating unity. eg: '2em'
-        },
-        onClick: function () { } // Callback after click
-    }).showToast();
+        gravity: "right", // Posición de la notificación (top, bottom, left, right) (opcional)
+        position: "right", // Posición dentro de la ubicación (opcional)
+        backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)", // Color de fondo (opcional)
+      }).showToast();
 
     const idBoton = e.currentTarget.id;
     const productoAgregado = productos.find(producto => producto.id === idBoton);
@@ -172,26 +160,13 @@ if (productosDetallesLS) {
 
 //Funcion que agrega los productos a favoritos
 function agregarAfavoritos(e) {
-
     Toastify({
         text: "Producto agregado a la lista de deseos",
         duration: 3000,
-        close: true,
-        gravity: "top", // `top` or `bottom`
-        position: "right", // `left`, `center` or `right`
-        stopOnFocus: true, // Prevents dismissing of toast on hover
-        style: {
-            background: "linear-gradient(to right, #458942,#528443)",
-            borderRadius: "2rem",
-            textTransform: "uppercase",
-            fontSize: ".75rem"
-        },
-        offset: {
-            x: '1.5rem', // horizontal axis - can be a number or a string indicating unity. eg: '2em'
-            y: '1.5rem' // vertical axis - can be a number or a string indicating unity. eg: '2em'
-        },
-        onClick: function () { } // Callback after click
-    }).showToast();
+        gravity: "right", // Posición de la notificación (top, bottom, left, right) (opcional)
+        position: "right", // Posición dentro de la ubicación (opcional)
+        backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)", // Color de fondo (opcional)
+      }).showToast();
 
     const idBoton = e.currentTarget.id;
     const productoAgregado = productos.find(producto => producto.id === idBoton);
